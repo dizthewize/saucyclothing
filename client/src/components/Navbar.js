e7f533bbeadf88dd6e538dc5385a9a8f99e6bca8
Unknown <martez_conner@outlook.com>
2018-03-14 13:09:18 -0700

    Fixed missing navbar links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,6 +16,12 @@ class NavBar extends Component {
 
     return this.props.token === '' ? 
     <div className="nav-links">
+      <ul className="nav-items left">
+        <li className="nav-item left"><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+        <li className="nav-item left"><NavLink to="/about" activeClassName="active">About</NavLink></li>
+        <li className="nav-item left"><NavLink to="/lookbook" activeClassName="active">LookBook</NavLink></li>
+      </ul>
+      
       <ul className="nav-items right">
         <li className="nav-item right"><NavLink to="/login" activeClassName="active">Login</NavLink></li>
         <li className="nav-item right"><NavLink to="/register" activeClassName="active">Register</NavLink></li>
